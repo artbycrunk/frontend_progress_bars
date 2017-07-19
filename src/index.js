3cc6353ae191147258f6dee8e75d1f97e122b3dc
Savio Fernandes <artbycrunk@gmail.com>
2017-07-20 03:12:53 +0800

    use fetch api to get data from endpoint

--- src/index.js
+++ src/index.js
@@ -2,17 +2,38 @@ import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import style from '../styles/style.scss';
 
+import 'whatwg-fetch'
 
 class App extends Component {
     constructor(props){
         super(props);
         this.state = {"buttons":[],"bars":[],"limit":100};
-        
+        this.getData();
     }
 
-    
+    getData() {
+        var _parent = this;
+        fetch('http://pb-api.herokuapp.com/bars.json')
+            .then(function(response) {
+                return response.json()
+            }).then(function(json) {
+                _parent.setState(json)
+                console.log('parsed json', json)
+            });
+    }
 
     render(){
+
+        if (!this.state.bars[0]){
+            return <div>Loading...</div>
+        }
+
+        return (
+            
+            <div>
+                <h1>Front End Assignment - Progress Bars</h1>
+            </div>
+        );
     }
 }
 
