cfafd7f7226b3c61afe0fbac348b1656da217951
Savio Fernandes <artbycrunk@gmail.com>
2017-07-20 03:33:16 +0800

    add progress controls jsx

--- src/components/progress_controls.js
+++ src/components/progress_controls.js
@@ -9,6 +9,29 @@ class ProgressControl extends Component {
     }
 
     render(){
+        
+        const progressBars = this.props.bars.map((bar, index) => {
+            return <option key={index} value={index}>#progress{index+1}</option>
+        });
+
+        const progressButtons = this.props.buttons.map((button, index) => {
+            return (
+                <button key={index} type="button" 
+                        value={button} 
+                        onClick={() => this.props.onButtonSelect(this.state.selected_bar,button)}>
+                    {((Math.sign(button) == 1) ? "+"+button : button)}
+                </button>
+            );
+        });
+
+        return (
+            <div className="progressbar-controls">
+                <select value={this.state.selected_bar}>
+                    {progressBars}
+                </select>
+                    {progressButtons}
+            </div>
+        );
     }
 }
 
